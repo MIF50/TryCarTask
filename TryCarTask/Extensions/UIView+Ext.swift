fd8f55fb9d41faa8defc7d5b1ecc6609d42c3318
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2021-05-16 11:37:13 +0300

    refactor UIView+Ext.

--- TryCarTask/Extensions/UIView+Ext.swift
+++ TryCarTask/Extensions/UIView+Ext.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIView {
     
     func addSubviews(_ views: UIView ...) {
-        for view in views { addSubview(view) }
+        views.forEach { addSubview($0) }
     }
 }
 
